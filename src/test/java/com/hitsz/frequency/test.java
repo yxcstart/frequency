0a8d85c9f9090fb739b85102c15c30b3374d7497
yangxianchang <1379254968@qq.com>
2021-04-27 15:55:25 +0800

    SSH传输

---
+++ src/test/java/com/hitsz/frequency/test.java
@@ -0,0 +1,11 @@
+package com.hitsz.frequency;
+
+/**
+ * @author yang
+ * @create 2021-04-27 15:54
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("push test");
+    }
+}
